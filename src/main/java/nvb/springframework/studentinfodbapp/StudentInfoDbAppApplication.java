36957e64bc73b72110c163bd2d5c4b78d5b95ad0
Erfan <erfannavab@gmail.com>
2022-10-19 08:53:40 +0330

    Initial commit

---
+++ src/main/java/nvb/springframework/studentinfodbapp/StudentInfoDbAppApplication.java
@@ -0,0 +1,13 @@
+package nvb.springframework.studentinfodbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInfoDbAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentInfoDbAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvb/springframework/studentinfodbapp/StudentInfoDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.springframework.studentinfodbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInfoDbAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
