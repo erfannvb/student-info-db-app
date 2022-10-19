d8f7c0845e85b1b3064810696a2fab12657d1ca8
Erfan <erfannavab@gmail.com>
2022-10-19 10:14:56 +0330

    - modified repository layer - modified service layer

--- src/main/java/nvb/springframework/studentinfodbapp/repository/StudentRepository.java
+++ src/main/java/nvb/springframework/studentinfodbapp/repository/StudentRepository.java
@@ -1,10 +1,10 @@
 package nvb.springframework.studentinfodbapp.repository;
 
+import nvb.springframework.studentinfodbapp.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository {
-
-
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
--- src/main/java/nvb/springframework/studentinfodbapp/service/StudentService.java
+++ src/main/java/nvb/springframework/studentinfodbapp/service/StudentService.java
@@ -1,6 +1,8 @@
 package nvb.springframework.studentinfodbapp.service;
 
+import lombok.AllArgsConstructor;
 import nvb.springframework.studentinfodbapp.model.Student;
+import nvb.springframework.studentinfodbapp.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,16 +10,13 @@ import java.time.Month;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(1L,
-                        "David",
-                        "david@yahoo.com",
-                        LocalDate.of(2000, Month.SEPTEMBER, 20),
-                        22)
-        );
+        return studentRepository.findAll();
     }
 
 }
